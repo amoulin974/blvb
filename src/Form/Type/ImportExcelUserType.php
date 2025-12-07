9d752e315e6b98d0d9a56fcf997255e4fd84e13e
Antoine Moulin <amoulin974@gmail.com>
2025-12-07 19:41:47 +0100

    ajout upload user avec feuille excel

---
+++ src/Form/Type/ImportExcelUserType.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
+class ImportExcelUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options):void
+    {
+        $builder->add('file', FileType::class, [
+            'label' => 'Fichier Excel à importer',
+            'mapped' => false,
+            'required' => true,
+            'constraints' => [
+                new File([
+                    'maxSize' => '5M',
+                    'mimeTypes' => [
+                        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                        'application/vnd.ms-excel',
+                        'text/csv',
+                    ],
+                    'mimeTypesMessage' => 'Merci d\'envoyer un fichier Excel ou csv valide',
+                ])
+            ]
+        ]);
+    }
+}
