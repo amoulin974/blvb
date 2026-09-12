82478bebd82efffdbdb55bec02ad3ae84a651e0f
patxmay <devmapluz@gmail.com>
2026-09-13 01:21:11 +0200

    [IMP]annulation modif

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,5 @@
 <?php
-
+// j'ai remis 'all' a la place de 'dev' pour Tailwind
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
