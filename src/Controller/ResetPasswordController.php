bfff997b5ab368adcd2e78039f6cf09bcc030c3d
patxmay <devmapluz@gmail.com>
2026-09-14 21:34:23 +0200

    [IMP]erreur email

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -157,7 +157,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('blvb@bvblc.fr', 'Admin reset password'))
+            ->from(new Address('blvb@vblc.fr', 'Admin reset password'))
             ->to((string) $user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
